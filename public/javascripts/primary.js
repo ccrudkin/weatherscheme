8a708f0ec622e846259350905ea0c34a9ce2193b
ccrudkin <ccrudkin@gmail.com>
2018-07-18 17:32:53 -0600

    beginning script functionality

--- public/javascripts/primary.js
+++ public/javascripts/primary.js
@@ -1,1 +1,24 @@
-console.log("Hello world");
+function getconditions() {
+    let coors = document.getElementById("locationInput").value;
+
+    // remove whitespace
+    let cleanCoors = coors.replace(" ", "");
+    console.log(cleanCoors);
+
+    /*
+    $.ajax({
+        url: "/getconditions", // use req.params here
+        type: "GET",
+        error
+    });
+    */
+}
+
+document.getElementById("submitButton").addEventListener("click", getconditions);
+// execute on enter press
+document.getElementById("locationInput").addEventListener("keypress", (event) => {
+    console.log(event.keyCode);
+    if (event.keyCode === 13) {
+        getconditions();
+    }
+});
